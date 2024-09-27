41c424ba242165af1fae0137e4d862681fe8d761
VerdantVibes <VerdantVibes@github.com>
2024-09-27 04:39:54 +0200

    fix: splitfullname

--- ptm_func.py
+++ ptm_func.py
@@ -178,3 +178,102 @@ def splitfullname(fullname):
             fullname = fullname.replace('MME. ', '')
         elif fullname.startswith("MME "):
             fullname = fullname.replace('MME ', '')
+            
+        # First remove any suffixes
+        if fullname.endswith(' JR'):
+            suffix = 'JR'
+            fullname = fullname[:-3]
+        elif fullname.endswith(' JR.'):
+            suffix = 'JR'
+            fullname = fullname[:-4]
+        elif fullname.endswith(' JUNIOR'):
+            suffix = 'JUNIOR'
+            fullname = fullname[:-7]
+        elif  fullname.endswith(' III'):
+            suffix = 'III'
+            fullname = fullname[:-4]
+        elif fullname.endswith(' II'):
+            suffix = 'II'
+            fullname = fullname[:-3]
+        elif fullname.endswith(' IV'):
+            suffix = 'IV'
+            fullname = fullname[:-3]
+        elif fullname.endswith(' SR'):
+            suffix = 'SR'
+            fullname = fullname[:-3]
+        elif fullname.endswith(' SR.'):
+            suffix = 'SR'
+            fullname = fullname[:-4]
+                                                                                                                                                                                                                        # Now handle any multi word last names
+        if ')' in fullname:
+            lastname = fullname.partition(')')[2]
+            firstname = fullname.partition(')')[0] + fullname.partition(')')[1] 
+        elif ' VAN ' in fullname:
+            lastname = fullname.partition(' VAN ')[1] + fullname.partition(' VAN ')[2]
+            firstname = fullname.partition(' VAN ')[0]
+        elif ' DER ' in fullname:
+            lastname = fullname.partition(' DER ')[1] + fullname.partition(' DER ')[2]
+            firstname = fullname.partition(' DER ')[0]
+        elif ' VANDER ' in fullname:
+            lastname = fullname.partition(' VANDER ')[1] + fullname.partition(' VANDER ')[2]
+            firstname = fullname.partition(' VANDER ')[0]
+        elif ' DE ' in fullname:
+            lastname = fullname.partition(' DE ')[1] + fullname.partition(' DE ')[2]
+            firstname = fullname.partition(' DE ')[0]
+        elif ' ABD ' in fullname:
+            lastname = fullname.partition(' ABD ')[1] + fullname.partition(' ABD ')[2]
+            firstname = fullname.partition(' ABD ')[0]
+        elif ' EL ' in fullname:
+            lastname = fullname.partition(' EL ')[1] + fullname.partition(' EL ')[2]
+            firstname = fullname.partition(' EL ')[0]
+        elif ' DEL ' in fullname:
+            lastname = fullname.partition(' DEL ')[1] + fullname.partition(' DEL ')[2]
+            firstname = fullname.partition(' DEL ')[0]                                     
+        elif ' DELA ' in fullname:
+    	    lastname = fullname.partition(' DELA ')[1] + fullname.partition(' DELA ')[2]
+    	    firstname = fullname.partition(' DELA ')[0]
+        elif ' DELLA ' in fullname:
+    	    lastname = fullname.partition(' DELLA ')[1] + fullname.partition(' DELLA ')[2]
+    	    firstname = fullname.partition(' DELLA ')[0]
+        elif ' DI ' in fullname:
+    	    lastname = fullname.partition(' DI ')[1] + fullname.partition(' DI ')[2]
+    	    firstname = fullname.partition(' DI ')[0]
+        elif ' DOS ' in fullname:
+            lastname = fullname.partition(' DOS ')[1] + fullname.partition(' DOS ')[2]
+            firstname = fullname.partition(' DOS ')[0]
+        elif ' DU ' in fullname:
+    	    lastname = fullname.partition(' DU ')[1] + fullname.partition(' DU ')[2]
+    	    firstname = fullname.partition(' DU ')[0]
+        elif  ' LE ' in fullname:
+    	    lastname = fullname.partition(' LE ')[1] + fullname.partition(' LE ')[2]
+    	    firstname = fullname.partition(' LE ')[0]
+        elif ' ST ' in fullname:
+            lastname = fullname.partition(' ST ')[1] + fullname.partition(' ST ')[2]
+            firstname = fullname.partition(' ST ')[0]	
+        elif ' ST. ' in fullname:
+            lastname = fullname.partition(' ST. ')[1] + fullname.partition(' ST. ')[2]
+            firstname = fullname.partition(' ST. ')[0]	
+        elif ' VON ' in fullname:
+            lastname = fullname.partition(' VON ')[1] + fullname.partition(' VON ')[2]
+            firstname = fullname.partition(' VON ')[0]	
+        elif ' DER ' in fullname:
+            lastname = fullname.partition(' DER ')[1] + fullname.partition(' DER ')[2]
+            firstname = fullname.partition(' DER ')[0]	
+        else:
+            firstname = fullname.rpartition(" ")[0]
+            lastname = fullname.rpartition(" ")[2]
+                   
+    # remove any leading or trailing spaces
+    firstname = firstname.strip()
+    lastname = lastname.strip()
+    
+    #to fix problem when the second character of the last name is a space
+    # ex: B PENICH
+    if len(lastname) > 1:
+        if lastname[1] == ' ':
+    	    #print('There is a space')
+    	    splitlastname = lastname.split(' ',1)
+    	    firstname = firstname + ' ' + splitlastname[0]
+    	    lastname = splitlastname[1]
+        
+    return firstname, lastname, suffix
